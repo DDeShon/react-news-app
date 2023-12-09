9f9b45dec6980303523d651e3d97c581a84b3ce9
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-12-09 12:07:09 -0600

    working on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ export const Navbar = () => {
     <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          Navbar
+          <span className="badge">NewsApp</span>
         </a>
         <button
           class="navbar-toggler"
