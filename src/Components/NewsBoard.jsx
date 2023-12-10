10a0db63590c89c2d46f2d82ff34cbb786b1409a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-12-10 12:02:29 -0600

    working on newsboard

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -5,7 +5,11 @@ export const NewsBoard = () => {
     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
       import.meta.env.VITE_API_KEY
     }`;
-  });
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles));
+  }, []);
+
   return (
     <div>
       <h2 className="text-center">
