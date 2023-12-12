07e8f40e1660ccdf98314241b1e8c74919ffbc5a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-12-11 18:25:39 -0600

    working on newsitem

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,11 +1,11 @@
 export const NewsItem = ({ title, description, src, url }) => {
   return (
     <div className="card" style="width: 18rem;">
-      <img src="..." className="card-img-top" alt="..." />
+      <img src={src} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{description}</p>
-        <a href="/" className="btn btn-primary">
+        <a href={url} className="btn btn-primary">
           Read More
         </a>
       </div>
