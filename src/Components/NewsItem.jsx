08ea395d4e8a7042176646d83a19a87b58ad485d
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-12-11 18:20:56 -0600

    working on newsitem

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -8,7 +8,7 @@ export const NewsItem = () => {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
-        <a href="#" className="btn btn-primary">
+        <a href="/" className="btn btn-primary">
           Go somewhere
         </a>
       </div>
